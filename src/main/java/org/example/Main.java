11c1a8dee97442a2de692c5e52aa07261e40d749
junwon_lee <wjdrbs307@naver.com>
2023-08-19 13:30:48 +0900

    Algorithm

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("Hello World!");
+
+        System.out.println(sb);
+        }
+
+}
---
+++ src/main/java/org/example/대소문자.java
@@ -0,0 +1,13 @@
+package org.example;
+
+
+public class 대소문자 {
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("Hello World!");
+
+        System.out.println(sb);
+        }
+
+}
