bd598affe188ac156eef3db23ffd11775babac43
junwon_lee <wjdrbs307@naver.com>
2025-05-20 23:36:42 +0900

    반복되지 않는 문자 1 문제 첫번째 내 풀이

--- 1st_week/01_05_find_not_repeating_first_character.py
+++ 1st_week/01_05_find_not_repeating_first_character.py
@@ -1,11 +1,17 @@
+from collections import Counter
+
 input = "abadabac"
 
 def find_not_repeating_first_character(string):
-    # 이 부분을 채워보세요!
+    counter = Counter(string)
+
+    for char in string:
+        if counter[char] == 1:
+            return char
     return "_"
 
 
 result = find_not_repeating_first_character
 print("정답 = d 현재 풀이 값 =", result("abadabac"))
 print("정답 = c 현재 풀이 값 =", result("aabbcddd"))
-print("정답 =_ 현재 풀이 값 =", result("aaaaaaaa"))
+print("정답 = _ 현재 풀이 값 =", result("aaaaaaaa"))
