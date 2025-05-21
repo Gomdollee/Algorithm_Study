4c22e7e9407801347eebcbec0709de129bb69d3d
junwon_lee <wjdrbs307@naver.com>
2025-05-21 22:55:16 +0900

    소수 나열하기 내풀이

--- 1st_week/01_08_find_prime_list_under_number.py
+++ 1st_week/01_08_find_prime_list_under_number.py
@@ -1,9 +1,17 @@
 input = 20
 
-
 def find_prime_list_under_number(number):
+    prime_list = []
+
+    for n in range(2, number+1):
+        for i in range(2, n):
+            if n % i == 0:
+                break
+        else:
+            prime_list.append(n)
+
     # 이 부분을 채워보세요!
-    return []
+    return prime_list
 
 
 result = find_prime_list_under_number(input)
