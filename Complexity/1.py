58b4c31a7cb6e5f695f4123c39b5d1b644a756e8
junwon_lee <wjdrbs307@naver.com>
2025-05-22 23:27:41 +0900

    복잡도 기본

--- Complexity/1.py
+++ Complexity/1.py
@@ -10,3 +10,11 @@ for i in array:
     for j in array:
         temp = i * j
         print(temp)
+
+
+# Big-O 표기법
+def sum_list(nums):
+    total = 0  # O(1) 한개임
+    for num in nums: # O(N) N만큼
+        total += num
+    return total #  O(1) 다시 한개
