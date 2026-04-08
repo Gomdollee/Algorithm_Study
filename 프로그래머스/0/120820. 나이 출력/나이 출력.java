5c7cab6f50c4d774d11c5c2182f6d2cb71ba2430
JunWon_Lee <99373291+Gomdollee@users.noreply.github.com>
2026-04-08 10:38:10 +0900

    [level 0] Title: 나이 출력, Time: 0.02 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int age) {
+        int answer = 0;
+        
+        int year = 2022; 
+        
+        answer = 2022-(age-1);
+        
+        return answer;
+        
+    }
+}
